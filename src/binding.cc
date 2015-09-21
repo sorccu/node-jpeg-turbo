659b24d2d5e1aff50a8741c9e0f4a81522f83a23
Simo Kinnunen <simo@shoqolate.com>
2015-09-21 11:31:53 +0900

    Support output format.

--- src/binding.cc
+++ src/binding.cc
@@ -8,36 +8,77 @@
 #define TJFLAG_FASTDCT 0
 #endif
 
+enum {
+  FORMAT_RGB  = TJPF_RGB,
+  FORMAT_BGR  = TJPF_BGR,
+  FORMAT_RGBX = TJPF_RGBX,
+  FORMAT_BGRX = TJPF_BGRX,
+  FORMAT_XRGB = TJPF_XRGB,
+  FORMAT_XBGR = TJPF_XBGR,
+  FORMAT_GRAY = TJPF_GRAY,
+  FORMAT_RGBA = TJPF_RGBA,
+  FORMAT_BGRA = TJPF_BGRA,
+  FORMAT_ABGR = TJPF_ABGR,
+  FORMAT_ARGB = TJPF_ARGB,
+};
+
 NAN_METHOD(DecompressSync) {
   int err;
   int width, height, jpegSubsamp;
   unsigned char* srcData;
   unsigned char* dstData;
-  size_t length;
+  uint32_t srcLength, dstLength;
+  uint32_t bpp;
 
-  if (info.Length() < 1) {
-    return Nan::ThrowError("Too few arguments");
+  if (info.Length() < 2) {
+    return Nan::ThrowError(Nan::TypeError("Too few arguments"));
   }
 
   v8::Local<v8::Object> srcObject = info[0].As<v8::Object>();
   srcData = (unsigned char*) node::Buffer::Data(srcObject);
-  length = node::Buffer::Length(srcObject);
+  srcLength = node::Buffer::Length(srcObject);
+
+  v8::Local<v8::Object> options = info[1].As<v8::Object>();
+  uint32_t format = options->Get(Nan::New("format").ToLocalChecked())->Uint32Value();
+
+  switch (format) {
+  case FORMAT_GRAY:
+    bpp = 1;
+    break;
+  case FORMAT_RGB:
+  case FORMAT_BGR:
+    bpp = 3;
+    break;
+  case FORMAT_RGBX:
+  case FORMAT_BGRX:
+  case FORMAT_XRGB:
+  case FORMAT_XBGR:
+  case FORMAT_RGBA:
+  case FORMAT_BGRA:
+  case FORMAT_ABGR:
+  case FORMAT_ARGB:
+    bpp = 4;
+    break;
+  default:
+    return Nan::ThrowError(Nan::TypeError("Invalid output format"));
+  }
 
   tjhandle dh = tjInitDecompress();
   if (dh == NULL) {
     return Nan::ThrowError(tjGetErrorStr());
   }
 
-  err = tjDecompressHeader2(dh, srcData, length, &width, &height, &jpegSubsamp);
+  err = tjDecompressHeader2(dh, srcData, srcLength, &width, &height, &jpegSubsamp);
   if (err != 0) {
     tjDestroy(dh);
     return Nan::ThrowError(tjGetErrorStr());
   }
 
-  v8::Local<v8::Object> dstObject = Nan::NewBuffer(width * height * 3).ToLocalChecked();
+  dstLength = width * height * bpp;
+  v8::Local<v8::Object> dstObject = Nan::NewBuffer(dstLength).ToLocalChecked();
   dstData = (unsigned char*) node::Buffer::Data(dstObject);
 
-  err = tjDecompress2(dh, srcData, length, dstData, width, 0, height, TJPF_RGB, TJFLAG_FASTDCT);
+  err = tjDecompress2(dh, srcData, srcLength, dstData, width, 0, height, format, TJFLAG_FASTDCT);
   if (err != 0) {
     tjDestroy(dh);
     return Nan::ThrowError(tjGetErrorStr());
@@ -52,6 +93,8 @@ NAN_METHOD(DecompressSync) {
   obj->Set(Nan::New("data").ToLocalChecked(), dstObject);
   obj->Set(Nan::New("width").ToLocalChecked(), Nan::New(width));
   obj->Set(Nan::New("height").ToLocalChecked(), Nan::New(height));
+  obj->Set(Nan::New("size").ToLocalChecked(), Nan::New(dstLength));
+  obj->Set(Nan::New("bpp").ToLocalChecked(), Nan::New(bpp));
 
   info.GetReturnValue().Set(obj);
 }
@@ -59,6 +102,17 @@ NAN_METHOD(DecompressSync) {
 NAN_MODULE_INIT(InitAll) {
   Nan::Set(target, Nan::New("decompressSync").ToLocalChecked(),
     Nan::GetFunction(Nan::New<v8::FunctionTemplate>(DecompressSync)).ToLocalChecked());
+  Nan::Set(target, Nan::New("FORMAT_RGB").ToLocalChecked(), Nan::New(FORMAT_RGB));
+  Nan::Set(target, Nan::New("FORMAT_BGR").ToLocalChecked(), Nan::New(FORMAT_BGR));
+  Nan::Set(target, Nan::New("FORMAT_RGBX").ToLocalChecked(), Nan::New(FORMAT_RGBX));
+  Nan::Set(target, Nan::New("FORMAT_BGRX").ToLocalChecked(), Nan::New(FORMAT_BGRX));
+  Nan::Set(target, Nan::New("FORMAT_XRGB").ToLocalChecked(), Nan::New(FORMAT_XRGB));
+  Nan::Set(target, Nan::New("FORMAT_XBGR").ToLocalChecked(), Nan::New(FORMAT_XBGR));
+  Nan::Set(target, Nan::New("FORMAT_GRAY").ToLocalChecked(), Nan::New(FORMAT_GRAY));
+  Nan::Set(target, Nan::New("FORMAT_RGBA").ToLocalChecked(), Nan::New(FORMAT_RGBA));
+  Nan::Set(target, Nan::New("FORMAT_BGRA").ToLocalChecked(), Nan::New(FORMAT_BGRA));
+  Nan::Set(target, Nan::New("FORMAT_ABGR").ToLocalChecked(), Nan::New(FORMAT_ABGR));
+  Nan::Set(target, Nan::New("FORMAT_ARGB").ToLocalChecked(), Nan::New(FORMAT_ARGB));
 }
 
 // There is no semi-colon after NODE_MODULE as it's not a function (see node.h).
